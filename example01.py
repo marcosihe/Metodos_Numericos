5de5cbc2789b699602793b1295ace02c984b238f
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-29 15:01:17 -0300

    example01.py contiene la forma de usar y declarar una matriz con la libreria numpy.ç

---
+++ example01.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def run():
+    a = np.array([1, 2, 3])
+    print('1D array')
+    print(a)
+
+    b = np.array([(1, 2, 3), (5, 6, 7)])
+    print('2D array')
+    print(b)
+
+
+if __name__ == '__main__':
+    run()
